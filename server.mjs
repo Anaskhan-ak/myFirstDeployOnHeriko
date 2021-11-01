826b646966eaad6d648cfb66bd8c5cc31739f603
Anaskhan-ak <56588005+Anaskhan-ak@users.noreply.github.com>
2021-11-01 19:58:04 +0500

    Add files via upload

---
+++ server.mjs
@@ -0,0 +1,20 @@
+import express from "express";
+const app = express()
+
+const port =process.env.PORT || 3000;
+
+app.get("/",(req,res) => {
+    res.send("Hey! Welcome To Node Js")
+})
+app.get("/home",(req,res) => {
+    res.send("Hey! Welcome To Home Page")
+})
+app.get("/profile",(req,res) => {
+    res.send("Hey! Welcome To Profile Page")
+})
+app.get("/about",(req,res) => {
+    res.send("Hey! Welcome To About Page")
+})
+ app.listen(port,()=>{
+     console.log("Server Is Running")
+ })
