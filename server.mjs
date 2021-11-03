46de392ff90cce3ee1c042376677c4e9741d59cd
Anaskhan-ak <56588005+Anaskhan-ak@users.noreply.github.com>
2021-11-03 19:37:00 +0500

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -2,9 +2,9 @@ import express from "express";
 const app = express()
 
 const port =process.env.PORT || 3000;
-
+let Akk = env.AKK
 app.get("/",(req,res) => {
-    res.send("Hey! Welcome To Node Js")
+    res.send("Hey! Welcome To Node Js",Akk)
 })
 app.get("/home",(req,res) => {
     res.send("Hey! Welcome To Home Page")
